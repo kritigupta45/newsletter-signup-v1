c5a510617818343567f587f221f292332e7fd4ca
Kriti Gupta <82928579+kritigupta45@users.noreply.github.com>
2022-07-29 16:10:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){    
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    }   
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/4fd5430360";
+
+    const options = {
+        method: "POST",
+        auth: "gitirk:072236dcb0ffd9a4de2eaca215b4d172-us14"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Started server on port 3000.");
+});
